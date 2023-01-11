919ff81571c20d36724627db1d714f4a1c386868
Sujen Shah <sujen1412@gmail.com>
2023-01-11 14:04:13 -0800

    correct MOZART_REST_URL var name

--- build.sh
+++ build.sh
@@ -19,5 +19,5 @@ if [ -z "${isSet}" ]; then
 fi
 CONTAINER_REGISTRY=localhost5050
 CACHE_BUST=$(date +%s)
-${VERDI_HOME}/ops/container-builder/build-container.bash ${REPO_NAME} ${BRANCH} ${S3_CODE_BUCKET} ${MOZART_REST_URL} ${GRQ_REST_URL} ${SKIP_PUBLISH} ${CONTAINER_REGISTRY} --build-arg BASE_IMAGE_NAME=${BASE_IMAGE_NAME} --build-arg REPO_URL_WITH_TOKEN=${REPO_URL_WITH_TOKEN} --build-arg REPO_NAME=${REPO_NAME} --build-arg BRANCH=${BRANCH} --build-arg BUILD_CMD="${BUILD_CMD}" --build-arg CACHE_BUST=${CACHE_BUST}
+${VERDI_HOME}/ops/container-builder/build-container.bash ${REPO_NAME} ${BRANCH} ${S3_CODE_BUCKET} ${MOZART_URL} ${GRQ_REST_URL} ${SKIP_PUBLISH} ${CONTAINER_REGISTRY} --build-arg BASE_IMAGE_NAME=${BASE_IMAGE_NAME} --build-arg REPO_URL_WITH_TOKEN=${REPO_URL_WITH_TOKEN} --build-arg REPO_NAME=${REPO_NAME} --build-arg BRANCH=${BRANCH} --build-arg BUILD_CMD="${BUILD_CMD}" --build-arg CACHE_BUST=${CACHE_BUST}
 
